7ed48bc382abec39adacfb4625aa148d03dcea7d
Akash Rajpurohit <akash009.dev@gmail.com>
2019-09-23 22:30:15 +0530

    :art: create new block method

--- blockchain.js
+++ blockchain.js
@@ -3,4 +3,20 @@ class Blockchain {
 		this.chain = [];
 		this.newTransactions = [];
 	}
+
+	createNewBlock(nonce, previousBlockHash, hash) {
+		const newBlock = {
+			index: this.chain.length + 1,
+			timestamp: Date.now(),
+			transactions: this.newTransactions,
+			nonce,
+			hash,
+			previousBlockHash
+		};
+
+		this.newTransactions = [];
+		this.chain.push(newBlock);
+
+		return newBlock;
+	}
 }
