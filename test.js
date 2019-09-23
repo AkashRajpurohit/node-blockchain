a4b9d5b5fc5870c1dd24eb5cbcb8f827c1260000
Akash Rajpurohit <akash009.dev@gmail.com>
2019-09-23 22:54:45 +0530

    :boom: create new transaction test

--- test.js
+++ test.js
@@ -3,7 +3,15 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2321, "adkljasldka", "asoidi011");
-bitcoin.createNewBlock(2123, "aslkdqwxqlkm", "iacjialncas");
-bitcoin.createNewBlock(4211, "0poqp31xnsakl", "laknsdlkasl");
+
+bitcoin.createNewTransaction(100, "ALEXHHUIBKJBKJH", "JENSDLKKSKXNL");
+
+bitcoin.createNewBlock(34112, "jasknkjajasas", "asjdnajsndjasl");
+
+bitcoin.createNewTransaction(50, "ALEXHHUIBKJBKJH", "JENSDLKKSKXNL");
+bitcoin.createNewTransaction(200, "ALEXHHUIBKJBKJH", "JENSDLKKSKXNL");
+bitcoin.createNewTransaction(3000, "ALEXHHUIBKJBKJH", "JENSDLKKSKXNL");
+
+bitcoin.createNewBlock(88294, "aslkjdaslkjlkxmlsk", "aSKJAjanJNLKKS");
 
 console.log(bitcoin);
