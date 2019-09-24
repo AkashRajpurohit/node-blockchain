164cf17b9af200c4ad8ede1ba4a47333938a52d5
Akash Rajpurohit <akash009.dev@gmail.com>
2019-09-24 10:50:38 +0530

    :art: basic api structure defined

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/blockchain', (req, res) => {
+
+});
+
+app.post('/transaction', (req, res) => {
+
+});
+
+app.get('/mine', (req, res) => {
+
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started at port: ${port}`))
