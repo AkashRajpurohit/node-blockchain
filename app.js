53b16783a771b87b2b9bf2e6d0c5bae10ef94d9c
Akash Rajpurohit <akash009.dev@gmail.com>
2019-09-24 11:35:10 +0530

    :sparkles: Bitcoin mining api done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const uuid = require('uuid/v1');
 
 const Blockchain = require('./lib/blockchain');
+
+const NODE_ADDRESS = uuid().split('-').join('');
 
 const app = express();
 
@@ -14,10 +17,32 @@ app.get('/blockchain', (req, res) => {
 });
 
 app.post('/transaction', (req, res) => {
+	const { amount, sender, recipient } = req.body;
+	const blockIndex = bitcoin.createNewTransaction(amount, sender, recipient);
 
+	res.json({ note: `Transaction will be added in block ${blockIndex}.`})
 });
 
 app.get('/mine', (req, res) => {
+	const lastBlock = bitcoin.getLastBlock();
+	const previousBlockHash = lastBlock['hash'];
+	const currentBlockData = {
+		transactions: bitcoin.pendingTransactions,
+		index: lastBlock['index'] + 1
+	};
+
+	const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+	const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+	// Reward this node
+	bitcoin.createNewTransaction(12.5, "00", NODE_ADDRESS);
+
+	const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+	res.json({
+		note: 'New block mined successfully',
+		block: newBlock
+	});
 
 });
 
